3c0eed2528a479b1f8982f14d9fe42a5aed5d6ef
vooooose <darina5.0069@gmail.com>
2022-08-19 13:34:46 +0200

    Домашнее задание 4
    Реализовала список покупок

--- script.js
+++ script.js
@@ -7,3 +7,29 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+const sendInput = document.querySelector('#input');
+const shoppingContainer = document.querySelector('#items');
+
+function addProduct() {
+    const product = sendInput.value;
+
+    const newProduct = document.createElement('p');
+    newProduct.textContent = product;
+
+    if (product != '') {
+        shoppingContainer.append(newProduct);
+    }
+
+    sendInput.value = '';
+
+    newProduct.addEventListener('click', function() {
+        newProduct.classList.toggle('done');
+    })
+}
+
+sendInput.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+        addProduct();
+    }
+})
